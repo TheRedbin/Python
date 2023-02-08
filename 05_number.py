1dd6ceefb40eea61e3a38c3620a95fb5448bcf9e
TheRedbin <maicolmanrique0@gmail.com>
2023-02-07 23:25:44 -0500

    Clase de numbers

---
+++ 05_number.py
@@ -0,0 +1,33 @@
+lives: int = 3
+print(type(lives))
+age: int = 24
+buget: int = 100
+
+temperature: float = 12.12
+print(type(temperature))
+
+lives: int = 2
+print(lives)
+lives: int = 1
+print(lives)
+
+lives: int = 12 + 15
+print(lives)
+
+lives: int = lives - 1
+print(lives)
+
+lives -= 1
+print(lives)
+
+lives -= 5
+print(lives)
+
+lives += 5
+print(lives)
+
+number: float = 450000000000000000000000.1
+print(number)
+
+number_b: float = 0.00000000000000001
+print(number_b)
