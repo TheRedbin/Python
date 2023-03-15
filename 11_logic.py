aed79d64b11acc44332ddf1cd9bf2651ae0a7c4f
Redbin <maicolmanrique0@gmail.com>
2023-03-14 21:57:20 -0500

    Quedé en el minuto 3:18 de operadores logicos and y or

---
+++ 11_logic.py
@@ -0,0 +1,6 @@
+# and
+print("AND")
+print("True and True ->", True and True)
+print("True and False ->", True and False)
+print("False and True ->", False and True)
+print("False and False ->", False and False)
