b6582b46a7fd8917638227db6fb0674296d43bd3
Redbin <maicolmanrique0@gmail.com>
2023-03-18 19:30:37 -0500

    Culminé tema sobre condicionales

--- 13_if.py
+++ 13_if.py
@@ -4,4 +4,24 @@ if True:
 if False:
 	print("no deberia ejecutarse")
 
+
 pet = input("¿Que animal tienes? ")
+
+if pet == "Perro":
+	print("Genial tienes buen gusto")
+
+elif pet == "Gato":
+	print("Espero tengas suerte")
+
+elif pet == "Pez":
+	print("Eres lo maximo")
+
+else:
+	print("No tienes ningun animal interesante")
+
+# stock = int(input("Digita el stock: "))
+
+# if stock >= 100 and stock <= 1000:
+# 	print("El stock esta en el rango correcto")
+# else:
+# 	print("El stock esta fuera de rango")
