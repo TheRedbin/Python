9df4310bb12ae341cd8a2f8b1ac8adc07dc95269
Redbin <maicolmanrique0@gmail.com>
2023-03-25 09:03:03 -0500

    Se cambiaron palabras a minuscula

--- main.py
+++ main.py
@@ -1,25 +1,25 @@
 user_option = input("¿Piedra, Papel o Tijera? -> ")
-user_option = user_option.capitalize()
-computer_option = "Papel"
+user_option = user_option.lower()
+computer_option = "papel"
 
 if user_option == computer_option:
     print("Empate")
-elif user_option == "Piedra":
-	if computer_option == "Tijera":
+elif user_option == "piedra":
+	if computer_option == "tijera":
 		print("Piedra gana a Tijera")
 		print("User Gana")
 	else:
 		print("Papel gana a Piedra")
 		print("Computer gana")
-elif user_option == "Papel":
-	if computer_option == "Piedra":
+elif user_option == "papel":
+	if computer_option == "piedra":
 		print("Papel gana a Piedra")
 		print("User gana")
 	else:
 		print("Tijeras gana a Papel")
 		print("Computer gana")
-elif user_option == "Tijera":
-	if computer_option == "Papel":
+elif user_option == "tijera":
+	if computer_option == "papel":
 		print("Tijera gana a Papel")
 		print("User gana")
 	else:
