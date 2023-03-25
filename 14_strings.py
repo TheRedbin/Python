f4c47ee6b929a86faa73b2e2be8b4cea7d6c897a
Redbin <maicolmanrique0@gmail.com>
2023-03-24 22:38:46 -0500

    Terminando clase de Strings recargados

--- 14_strings.py
+++ 14_strings.py
@@ -1,2 +1,28 @@
 text = "Ella sabe programar en Python"
-print("Ella" in text)
+"""
+print("JavaScript" in text)
+print("Python" in text)
+
+if 'Python' in text:
+    print("Eligiste bien!!")
+else:
+    print("Tambien elegiste bien")
+"""
+size = len("amor ")
+print(f"La cantidad de caracteres es de -> {size}")
+
+
+print(text.upper())
+print(text.lower())
+print(text.count("a"))
+print(text.swapcase())
+print(text.startswith("Ella"))
+print(text.endswith("lol"))
+print(text.replace("Ella", "Python"))
+
+text_2 = "este es un titulo"
+print(text_2)
+print(text_2.capitalize())
+print(text_2.title())
+print(text.isdigit())
+print("25".isdigit())
